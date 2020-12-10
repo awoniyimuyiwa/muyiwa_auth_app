099b7cbb9b4232cd3f77e65661a1c35c0a02d8d0
Muyiwa <muyiwaawoniyi@yahoo.com>
2020-12-10 06:26:31 +0100

    docker fixes

--- dbmigration/docker-entrypoint.sh
+++ dbmigration/docker-entrypoint.sh
@@ -1,28 +1,22 @@
 #!/usr/bin/env bash
 
 # Set bash script processing to fail on the first encountered error
-#set -e
+set -e
 
 server=${SERVER:?"SERVER environment variable must be set"}
 password=${SA_PASSWORD:?"SA_PASSWORD environment variable must be set"}
 timeout=${TIMEOUT:-300} 
 
-/wait-for-it.sh -t "$timeout" "$server:1433"
-if  [$? -ne 0] 
+if /wait-for-it.sh -t "$timeout" "$server:1433" 
 then
     echo >&2 "Unable to connect to $server. Aborting."
-    exit
+    exit 1
 fi   
 
-for file in /scripts/*.sql; do
+for file in /scripts/*.sql
+do
     /opt/mssql-tools/bin/sqlcmd -S "$server" -U sa -P "$password" -d master -i "$file"
-    if  [$? -eq 0]
-    then
-       echo "Exceuted $file"
-       break
-    else 
-        sleep 1
-    fi
+    echo "Exceuted $file"
 done
 
 echo "Database setup completed"
